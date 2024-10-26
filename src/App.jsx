0dd7b85614636b54ab83106311c3e0590ef87ca1
sahil kumar <Sk8877532873@gmail.com>
2024-10-26 12:10:50 +0530

    Delivery page is added with distance calculater

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
-import HomePage from './Component/homepage';
-
+import Rent from '../src/Component/Rent'
+import Delivery from '../src/Component/Delivery'
+import HomePage from '../src/Component/HomePage'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Delivery from './Component/Delivery';
-import Rent from './Component/Rent';
+
 
 export default function App() {
   return (
